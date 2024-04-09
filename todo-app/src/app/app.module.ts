930c4f62b33307ab2b8e402ef0a42859e43cfe60
95167 <95167@tyazhmash.com>
2024-04-09 09:07:05 +0400

    todo-app

---
+++ todo-app/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToDoListComponent } from './Components/to-do-list/to-do-list.component';
+
+@NgModule({
+  declarations: [AppComponent, ToDoListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
