e65c426d484c9cede7240d8e09d26c51922da7fd
95167 <95167@tyazhmash.com>
2024-07-29 11:41:00 +0400

    ДЗ10

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-app';
+  constructor(private location: Location) {}
+
+  switchLanguage(language: string) {
+    const url = this.location.path().split('?')[0];
+    window.location.href = `${url}?lang=${language}`;
+  }
 }
